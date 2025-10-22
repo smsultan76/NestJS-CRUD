39b12771766a2fd03b027dd791b79587a1f0b32a
SULTANUM MOBIN <sultanum.mobin@gmail.com>
2025-10-22 22:47:50 +0600

    Created Users Modules

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Delete, Get, Param, Post, Put } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Post, Put, Query } from '@nestjs/common';
 
 @Controller('users')
 export class UsersController {
@@ -9,8 +9,8 @@ export class UsersController {
     ]
 
     @Get()
-    getAllUsers() {
-        return this.users;
+    getAllUsers(@Query('role') role?: 'Intern' | 'Engineer' | 'Admin' ) {
+        return [ this.users, role ?? 'No role'];
     }
 
     @Get(':id')
