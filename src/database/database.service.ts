3cf782709b756a0b6e0a7dc41095153b834aa094
SULTANUM MOBIN <sultanum.mobin@gmail.com>
2025-10-23 18:02:48 +0600

    error fixed

--- src/database/database.service.ts
+++ src/database/database.service.ts
@@ -2,8 +2,8 @@ import { Injectable, OnModuleInit } from '@nestjs/common';
 import { PrismaClient } from '@prisma/client';
 
 @Injectable()
-export class DatabaseService extends PrismaClient inplements OnModuleInit {
+export class DatabaseService extends PrismaClient implements OnModuleInit {
     async onModuleInit(){
-        await this.#connect();
+        await this.$connect();
     }
 }
