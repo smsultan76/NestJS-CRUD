1e36f779b50146f0c423dce0dab174b47589f6c9
SULTANUM MOBIN <sultanum.mobin@gmail.com>
2025-10-24 19:25:52 +0600

    Profile Create() with file + validation

--- src/profile/profile.controller.ts
+++ src/profile/profile.controller.ts
@@ -1,4 +1,30 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post, UploadedFile, UseInterceptors } from '@nestjs/common';
+import { ProfileService } from './profile.service';
+import { FileInterceptor } from '@nestjs/platform-express';
+import { CreateProfileDto } from './dto/create-profile.dto';
+import { diskStorage } from 'multer';
+import { extname } from 'path';
 
 @Controller('profile')
-export class ProfileController {}
+export class ProfileController {
+    constructor(private readonly profileService: ProfileService) { }
+
+    //Create with file upload
+    @Post()
+    @UseInterceptors(
+        FileInterceptor('file', {
+            storage: diskStorage({
+                destination: './uploads/profile_pics',
+                filename: (req, file, cb) => {
+                    // Fix: 'originalname' not 'orginalname'
+                    const unique = Date.now() + '-' + Math.round(Math.random() * 1e9);
+                    cb(null, `${unique}${extname(file.originalname)}`);
+                },
+            }),
+        }),
+    )
+    create(@Body() dto: CreateProfileDto, @UploadedFile() file: Express.Multer.File) {
+        if (file) dto.profile_url = `/uploads/profile_pics/${file.filename}`;
+        return this.profileService.create(dto);
+    }
+}
--- src/profile/profile.service.ts
+++ src/profile/profile.service.ts
@@ -1,4 +1,12 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaService } from 'src/prisma/prisma.service';
+import { CreateProfileDto } from './dto/create-profile.dto';
 
 @Injectable()
-export class ProfileService {}
+export class ProfileService {
+    constructor(private prisma: PrismaService) {}
+
+    create(data: CreateProfileDto){
+        return this.prisma.profile.create({ data });
+    }
+}
